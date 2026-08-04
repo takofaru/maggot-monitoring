644833a23f6fcc04393421bfddf7f568d6e3dad1
fadelior@gmail.com <78681226+takofaru@users.noreply.github.com>
2026-08-04 23:34:30 +0700

    Add some pages

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,3 @@
+<div>
+    This is dashboard   <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+</div>
---
+++ resources/views/portal.blade.php
@@ -0,0 +1,3 @@
+<div>
+    This is login portal   <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
+</div>
