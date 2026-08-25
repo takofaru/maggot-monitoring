31c47b9ba81b7eabd88b1162da388a0cdd40aa6e
takofaru <78681226+takofaru@users.noreply.github.com>
2026-08-25 14:31:18 +0700

    fix stupid header (pls work)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
